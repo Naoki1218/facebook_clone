5619bc4b0dfdbd698b0aab5785768796c725cccf
nakajima naoki <naoki9890@yahoo.co.jp>
2020-06-30 13:35:47 +0900

    (:thumb)追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :blogs do
-  # resources :feeds do
     collection do
       post :confirm
     end
